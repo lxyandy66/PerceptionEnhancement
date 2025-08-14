2849ac9cd7d804847e1050c0ab7648c58df9ff9d
Li Xinyue <lxy_2008focus@126.com>
2025-08-14 18:50:46 +0800

    大幅简化了使用中采样的方式

--- PE_WindowSensor.ino
+++ PE_WindowSensor.ino
@@ -4,6 +4,7 @@
 
 #include <Arduino.h>
 #include <IOIndex.h>
+#include <CtrlBoardIndex.h>
 #include <AgentProtocol.h>
 #include <ArduinoJson.h>
 #include "IoTCtrlBoardManager.h"
@@ -11,13 +12,14 @@
 #include "ResistanceMapper.h"
 #include <max6675.h>
 #include<Chrono.h>
+#include <vector>
 
 /****电路定义****/
 #define PIN_MEASUREMENT_ITO A1
 #define PIN_MEASUREMENT_AgNW A2
 #define PIN_MEASUREMENT_PTIN A3
 #define PIN_MEASUREMENT_PTOUT A4
-#define PIN_ENABLE D10
+#define PIN_ENABLE D13
 #define SWITCH_ENABLE LOW    // 低电平使能
 #define SWITCH_DISABLE HIGH  // 低电平使能
 
@@ -25,22 +27,29 @@
 #define ANALOG_RESOLUTION_MAX 65535  // 模拟测量分辨率的最大值
 #define TIME_STARTUP 100             // 使能后至采样的等待时间
 
+#define VNAME_R_ITO "R_ITO"
+#define VNAME_R_AgNW "R_AgNW"
+#define VNAME_L_IN "L_IN"
+#define VNAME_L_OUT "L_OUT"
+#define VNAME_T_IN "T_IN"
+#define VNAME_T_OUT "T_OUT"
+
+
 #define SAMPLING_INTERVAL 1000
 
 StaticJsonDocument<1024> jsonDoc; //负责格式化输出
+std::vector<CtrlAccessory*> ctrlAccContainer;
 
-ResistanceMapper rMapper(0,"M_ITO");
-MAX6675 thermalCouple_in(D2,D3,D4); //SCLK,CS,MISO
-MAX6675 thermalCouple_out(D2,D3,D4); //SCLK,CS,MISO
-
-double temperture = -999;
+PackedMAX6675 tcIn("TC_in", "T_IN",(new MAX6675(D2, D3, D4)));
+PackedMAX6675 tcOut("TC_out", "T_OUT",(new MAX6675(D2, D5, D4)));
+AnalogReader srITO("ITO",VNAME_R_ITO,PIN_MEASUREMENT_ITO,ANALOG_RESOLUTION,3);
+AnalogReader srAgNW("AgNW",VNAME_R_AgNW,PIN_MEASUREMENT_AgNW,ANALOG_RESOLUTION,3);
+AnalogReader slIn("PT_IN",VNAME_L_IN,PIN_MEASUREMENT_PTIN,ANALOG_RESOLUTION,3);
+AnalogReader slOut("PT_OUT",VNAME_L_OUT,PIN_MEASUREMENT_PTOUT,ANALOG_RESOLUTION,3);
 
 long loopCount = 0;  // 循环计数用
-long readWinSensor_ITO = 0;
-long readWinSensor_AgNW = 0;
-long readPtSensor_in = 0;
-long readPtSensor_out = 0;
-String temp = "";
+
+String jsonOutString = "";
 
 Chrono sampleChrono;
 
@@ -50,51 +59,41 @@ void setup() {
   analogReadResolution(ANALOG_RESOLUTION);
   analogWriteResolution(ANALOG_RESOLUTION);
 
+  //传感器压入vector
+  ctrlAccContainer.push_back(&tcIn);
+  ctrlAccContainer.push_back(&tcOut);
+  ctrlAccContainer.push_back(&srITO);
+  ctrlAccContainer.push_back(&srAgNW);
+  ctrlAccContainer.push_back(&slIn);
+  ctrlAccContainer.push_back(&slOut);
+
   pinMode(PIN_ENABLE, OUTPUT);
 
   Serial.println("Setup finished!");
 }
 
 void loop() {
-
-  
   if (sampleChrono.hasPassed(SAMPLING_INTERVAL)) {
 
     /****输出内容重置****/
     sampleChrono.restart();
     jsonDoc.clear();
-    temp = "";
-
+    jsonOutString = "";
+    jsonDoc[AgentProtocol::REQ_ID_FROM_JSON] = loopCount;
     /**** 使能读取 ****/
     digitalWrite(PIN_ENABLE, SWITCH_ENABLE);  // 串联继电器连通 使能
     delay(TIME_STARTUP);
-    // 读取
-    readWinSensor_ITO = analogRead(PIN_MEASUREMENT_ITO);
-    readWinSensor_AgNW = analogRead(PIN_MEASUREMENT_AgNW);
     // 读取部分
+    for (int i = 0; i < ctrlAccContainer.size();i++){
+      ctrlAccContainer[i]->outputStatus(&jsonDoc, true, true);
+    }
     digitalWrite(PIN_ENABLE, SWITCH_DISABLE);
     /**** 使能读取结束 ****/
+
     
-    /**** 模拟量读取 ****/
-    readPtSensor_in = analogRead(PIN_MEASUREMENT_PTIN);
-    readPtSensor_out = analogRead(PIN_MEASUREMENT_PTOUT);
-    
-    /**** 温度读取 ****/
     /**** 打包输出 ****/
-    jsonDoc[AgentProtocol::DEV_ID_FROM_JSON] = "win"; //整个的窗户名
-    jsonDoc[AgentProtocol::REQ_ID_FROM_JSON] = loopCount;
-
-    jsonDoc["R_ITO"] = readWinSensor_ITO;
-    jsonDoc["R_AgNW"] = readWinSensor_AgNW;
-    jsonDoc["L_IN"] = readPtSensor_in;
-    jsonDoc["L_OUT"] = readPtSensor_out;
-    jsonDoc["T_IN"] = thermalCouple_in.readCelsius();
-    jsonDoc["T_OUT"] = thermalCouple_out.readCelsius();
-
-    delay(1000);
-
-    serializeJson(jsonDoc, temp);
-    Serial.println(temp);
+    serializeJson(jsonDoc, jsonOutString);
+    Serial.println(jsonOutString);
     loopCount++;
   }
 }
