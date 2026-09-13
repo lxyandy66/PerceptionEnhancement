588824cf2fd21788c273648bace7fe225015411c
Li Xinyue <lxy_2008focus@126.com>
2026-09-13 22:47:34 +0800

    修正了RateL的中线拟合，不再使用手动方式

--- PE_Characterizator.R
+++ PE_Characterizator.R
@@ -28,6 +28,13 @@ write.csv(data.pe.ecs.pickup,file="PE_ECS_PickUp.csv",row.names = FALSE,na = "")
 
 
 #### Agent选取数据可视化 ####
+#### 总体可视化 ####
+data.pe.ecs.post.tin<-fread("/Users/Mr_Li/Documents/博后课题项目/PerceptionEnhancement/AgentDataProcessor/PE_ECS_Full_Tin_REV.csv",data.table=TRUE)
+data.pe.ecs.post.rateL<-fread("/Users/Mr_Li/Documents/博后课题项目/PerceptionEnhancement/AgentDataProcessor/PE_ECS_Full_RateL_REV.csv",data.table=TRUE)
+table(data.pe.ecs.post.rateL$source_folder)
+
+ggplot(data=data.pe.ecs.post.rateL,aes(x=resistance,y=Rate_L_norm))+geom_point(position="jitter")+facet_wrap(~source_folder,nrow=3)
+
 
 data.pe.ecs.post.tin.draft<-fread("/Users/Mr_Li/Documents/博后课题项目/PerceptionEnhancement/AgentDataProcessor/过程可视化文件/annotated_temp.csv",data.table=TRUE)
 data.pe.ecs.post.rateL.draft<-fread("/Users/Mr_Li/Documents/博后课题项目/PerceptionEnhancement/AgentDataProcessor/过程可视化文件/annotated_rateL.csv",data.table=TRUE)
@@ -56,18 +63,30 @@ fit<-lm(resistance~temp_bin_ctr,data = data.pe.ecs.tave.mid[source_folder=="CY1_
 summary(fit)
 
 #### RateL处理 ####
-data.pe.ecs.rateL.mid<-data.pe.ecs.post.rateL.draft[,.(source_folder=source_folder[1],
-                                                rateL_res_bin=rateL_res_bin[1], # X坐标，电阻的分箱
-                                                resistance=mean(resistance,na.rm=TRUE),
-                                                Rate_L_norm=mean(Rate_L_norm,na.rm=TRUE) #透光率
-                                                ),by=(labelSampleRateLbin=paste0(source_folder,rateL_res_bin))]
+data.pe.ecs.rateL.mid<-fread("/Users/Mr_Li/Documents/博后课题项目/PerceptionEnhancement/AgentDataProcessor/过程可视化文件/rateL_midline.csv",data.table = TRUE)
+# 直接读取中线文件 这里处理的方式，S段比较特殊
 
-setorder(data.pe.ecs.tave.mid,source_folder,temp_bin_ctr)
+# data.pe.ecs.post.rateL.draft[,.(source_folder=source_folder[1],
+#                                 rateL_res_bin=rateL_res_bin[1], # X坐标，电阻的分箱
+#                                 resistance=mean(resistance,na.rm=TRUE),
+#                                 Rate_L_norm=mean(Rate_L_norm,na.rm=TRUE) #透光率
+#                                 ),by=(labelSampleRateLbin=paste0(source_folder,rateL_res_bin))]
+data.pe.ecs.rateL.mid<-data.pe.ecs.rateL.mid[branch=="full"][,-"branch"]
+setorder(data.pe.ecs.rateL.mid,sample,resistance)
+
+# 单独文件输出
+for(i in unique(data.pe.ecs.rateL.mid$sample)){
+    write.csv(data.pe.ecs.rateL.mid[sample==i],file=paste0(i,"_RateL_Line_forFig.csv"),row.names = FALSE,na = "")
+}
+for(i in unique(data.pe.ecs.post.rateL.draft$source_folder)){
+    write.csv(data.pe.ecs.post.rateL.draft[source_folder==i,-c("kept15","rateL_res_bin")],file=paste0(i,"_ECS_RateL_forFig.csv"),row.names = FALSE,na = "")
+}
+    
 
 ggplot(data.pe.ecs.post.rateL.draft[source_folder=="AA1_ECS"],aes(x=resistance,y=Rate_L_norm,color=isHeating))+geom_point()
-
 ggplot(data.pe.ecs.rateL.mid[source_folder=="AA1_ECS"],aes(x=rateL_res_bin,y=Rate_L_norm))+geom_point()
 
-fit.pe.ecs.r2l<-glm(Rate_L_norm~rateL_res_bin,data = data.pe.ecs.rateL.mid[source_folder=="AA1_ECS"],family = quasibinomial)
+# 用中线文件直接拟合没问题，似乎原始文件用分箱聚合有点问题
+fit.pe.ecs.r2l<-glm(Rate_L_norm~log10(resistance),data = data.pe.ecs.rateL.mid[sample=="AA1_ECS"&branch=="full"],family = quasibinomial) #quasi
 summary(fit.pe.ecs.r2l)
-PseudoR2(fit.pe.ecs.r2l)
+PseudoR2(fit.pe.ecs.r2l) #用的是Efron 伪R2
